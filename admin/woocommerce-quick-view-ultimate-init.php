f6ddb7157abfeb4b3ef6a99d54cebe3be2c99c41
a3rev <a3rev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-04 05:12:45 +0000

    Created tag 1.2.3.
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-products-quick-view/tags/1.2.3@1174150 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/woocommerce-quick-view-ultimate-init.php
+++ admin/woocommerce-quick-view-ultimate-init.php
@@ -6,13 +6,13 @@ update_option('wc_quick_view_ultimate_plugin', 'wc_quick_view_ultimate');
 function wc_quick_view_ultimate_install(){
 	global $wpdb;
 	//global $wp_rewrite;
-	
+
 	// Set Settings Default from Admin Init
 	global $wc_qv_admin_init;
 	$wc_qv_admin_init->set_default_settings();
-	
-	update_option('wc_quick_view_ultimate_version', '1.0.6');
-	update_option('wc_quick_view_lite_version', '1.0.5');
+
+	update_option('wc_quick_view_ultimate_version', '1.2.3');
+	update_option('wc_quick_view_lite_version', '1.2.3');
 	update_option('wc_quick_view_ultimate_plugin', 'wc_quick_view_ultimate');
 	//$wp_rewrite->flush_rules();
 	update_option('wc_quick_view_ultimate_just_installed', true);
@@ -33,6 +33,9 @@ add_action('init', 'quick_view_ultimate_init');
 // Add custom style to dashboard
 add_action( 'admin_enqueue_scripts', array( 'WC_Quick_View_Ultimate', 'a3_wp_admin' ) );
 
+// Add admin sidebar menu css
+add_action( 'admin_enqueue_scripts', array( 'WC_Quick_View_Ultimate', 'admin_sidebar_menu_css' ) );
+
 // Add text on right of Visit the plugin on Plugin manager page
 add_filter( 'plugin_row_meta', array('WC_Quick_View_Ultimate', 'plugin_extra_links'), 10, 2 );
 
@@ -45,7 +48,7 @@ $GLOBALS['wc_quick_view_ultimate_ultimate'] = new WC_Quick_View_Ultimate();
 // Check upgrade functions
 add_action('plugins_loaded', 'wc_quick_view_lite_upgrade_plugin');
 function wc_quick_view_lite_upgrade_plugin () {
-	
+
 	// Upgrade to 1.0.2
 	if ( version_compare(get_option('wc_quick_view_ultimate_version'), '1.0.2' ) === -1) {
 		$quick_view_ultimate_on_hover_bt_border_width = get_option( 'quick_view_ultimate_on_hover_bt_border_width' );
@@ -60,7 +63,7 @@ function wc_quick_view_lite_upgrade_plugin () {
 			'rounded_value' => $quick_view_ultimate_on_hover_bt_rounded
 		);
 		update_option( 'quick_view_ultimate_on_hover_bt_border', $quick_view_ultimate_on_hover_bt );
-		
+
 		$quick_view_ultimate_on_hover_bt_font_family = get_option( 'quick_view_ultimate_on_hover_bt_font_family' );
 		$quick_view_ultimate_on_hover_bt_font_size = get_option( 'quick_view_ultimate_on_hover_bt_font_size' );
 		$quick_view_ultimate_on_hover_bt_font_style = get_option( 'quick_view_ultimate_on_hover_bt_font_style' );
@@ -72,7 +75,7 @@ function wc_quick_view_lite_upgrade_plugin () {
 			'color' 	=> $quick_view_ultimate_on_hover_bt_font_color
 		);
 		update_option( 'quick_view_ultimate_on_hover_bt_font', $quick_view_ultimate_on_hover_bt_font );
-		
+
 		$quick_view_ultimate_on_hover_bt_enable_shadow = get_option( 'quick_view_ultimate_on_hover_bt_enable_shadow' );
 		$quick_view_ultimate_on_hover_bt_shadow_color = get_option( 'quick_view_ultimate_on_hover_bt_shadow_color' );
 		$quick_view_ultimate_on_hover_bt_shadow = array(
@@ -83,14 +86,14 @@ function wc_quick_view_lite_upgrade_plugin () {
 			'spread'	=> '0px',
 			'color'		=> $quick_view_ultimate_on_hover_bt_shadow_color,
 			'inset'		=> '',
-			
+
 		);
 		update_option( 'quick_view_ultimate_on_hover_bt_shadow', $quick_view_ultimate_on_hover_bt_shadow );
-		
+
 		$quick_view_ultimate_on_hover_bt_transparent = get_option( 'quick_view_ultimate_on_hover_bt_transparent' );
 		$quick_view_ultimate_on_hover_bt_transparent = $quick_view_ultimate_on_hover_bt_transparent * 10;
 		update_option( 'quick_view_ultimate_on_hover_bt_transparent', $quick_view_ultimate_on_hover_bt_transparent );
-		
+
 		$quick_view_ultimate_under_image_link_font_family = get_option( 'quick_view_ultimate_under_image_link_font_family' );
 		$quick_view_ultimate_under_image_link_font_size = get_option( 'quick_view_ultimate_under_image_link_font_size' );
 		$quick_view_ultimate_under_image_link_font_style = get_option( 'quick_view_ultimate_under_image_link_font_style' );
@@ -102,7 +105,7 @@ function wc_quick_view_lite_upgrade_plugin () {
 			'color' 	=> $quick_view_ultimate_under_image_link_font_color
 		);
 		update_option( 'quick_view_ultimate_under_image_link_font', $quick_view_ultimate_under_image_link_font );
-		
+
 		$quick_view_ultimate_under_image_bt_border_width = get_option( 'quick_view_ultimate_under_image_bt_border_width' );
 		$quick_view_ultimate_under_image_bt_border_style = get_option( 'quick_view_ultimate_under_image_bt_border_style' );
 		$quick_view_ultimate_under_image_bt_border_color = get_option( 'quick_view_ultimate_under_image_bt_border_color' );
@@ -115,7 +118,7 @@ function wc_quick_view_lite_upgrade_plugin () {
 			'rounded_value' => $quick_view_ultimate_under_image_bt_rounded
 		);
 		update_option( 'quick_view_ultimate_under_image_bt_border', $quick_view_ultimate_under_image_bt_border );
-		
+
 		$quick_view_ultimate_under_image_bt_font_family = get_option( 'quick_view_ultimate_under_image_bt_font_family' );
 		$quick_view_ultimate_under_image_bt_font_size = get_option( 'quick_view_ultimate_under_image_bt_font_size' );
 		$quick_view_ultimate_under_image_bt_font_style = get_option( 'quick_view_ultimate_under_image_bt_font_style' );
@@ -127,12 +130,12 @@ function wc_quick_view_lite_upgrade_plugin () {
 			'color' 	=> $quick_view_ultimate_under_image_bt_font_color
 		);
 		update_option( 'quick_view_ultimate_under_image_bt_font', $quick_view_ultimate_under_image_bt_font );
-			
+
 		update_option('wc_quick_view_ultimate_version', '1.0.2');
 	}
-	
-	update_option('wc_quick_view_ultimate_version', '1.0.6');
-	update_option('wc_quick_view_lite_version', '1.0.5');
+
+	update_option('wc_quick_view_ultimate_version', '1.2.3');
+	update_option('wc_quick_view_lite_version', '1.2.3');
 
 }
 ?>
